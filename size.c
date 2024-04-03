3e75bf410e3419bbbd95f148bb9631f9b0c920f8
goodyoung <goodyoung0615@gmail.com>
2024-04-03 09:51:38 +0900

    Update: size file

--- size.c
+++ size.c
@@ -9,7 +9,7 @@ int main() {
     // 포인터 변수의 경우 64bit machine에서 8byte를 차지하기 때문에 8이 출력된다.
 
     printf("sizeof(*x) = %lu\n", sizeof(*x)); // 정수형 2중 포인터 변수 x가 한 번 dereference가 되어서 메모리에서 차지하는 byte값을 long 데이터 타입으로 출력
-    // 한 번 dereference가 되어도 여전히 포인터 변수이기 때문에 포인터 변수의 경우 64bit machine에서 8byte를 차지하기 때문에 8이 출력된다.
+    // 2중 포인터가 한 번 dereference가 되어도 여전히 포인터 변수이기 때문에 포인터 변수의 경우 64bit machine에서 8byte를 차지하기 때문에 8이 출력된다.
 
     printf("sizeof(**x) = %lu\n", sizeof(**x)); // 정수형 2중 포인터 변수 x가 두 번 dereference가 되어서 메모리에서 차지하는 byte값을 long 데이터 타입으로 출력
     // 두 번 dereference가 되면 저장되어있는 값을 나타내기 때문에 정수형 데이터 값이 차지하는 4bytes가 출력이 된다.
